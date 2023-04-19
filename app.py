ebfc1236214308d2eb0ab5d2fd368cb5111fba06
visioncairgroup <Vision-CAIR@users.noreply.huggingface.co>
2023-04-19 22:25:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ with gr.Blocks() as demo:
     )
     clear.click(gradio_reset, [chat_state, img_list], [chatbot, image, text_input, upload_button, chat_state, img_list], queue=False)
 
-demo.launch(share=True, enable_queue=True)
+demo.launch(enable_queue=True)
